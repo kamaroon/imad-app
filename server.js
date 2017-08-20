a3563f29c5bf726cd314a6b66e50c558a48a5cc2
kamaroon <kamaroon.rbk@gmail.com>
2017-08-20 23:27:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("my second page response");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("my three page response");
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
